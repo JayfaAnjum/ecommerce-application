c91de90ec6fc619f5983e919944ea103de16f4d0
JayfaAnjum <jayfatha62@gmail.com>
2026-01-05 00:25:39 +0530

    done

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,9 @@
 const serverless = require('serverless-http');
 const app = require('./app');
-const connectDatabase = require('./config/database');
 
-let isDbConnected = false;
-
-// Handler function for Vercel
-const handler = async (req, res) => {
-  try {
-    // Connect to DB only once
-    if (!isDbConnected) {
-      await connectDatabase();
-      isDbConnected = true;
-      console.log('Database connected');
-    }
-
-    // Pass the request to Express app
-    return app(req, res);
-  } catch (err) {
-    console.error('Error in handler:', err);
-    res.status(500).json({ success: false, message: 'Internal Server Error' });
-  }
+// Simple handler without DB
+const handler = (req, res) => {
+  return app(req, res);
 };
 
 module.exports = serverless(handler);
