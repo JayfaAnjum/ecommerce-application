117edf00af49ec4775e2acbeb700739276cf5166
JayfaAnjum <jayfatha62@gmail.com>
2026-01-05 12:00:13 +0530

    error solved

--- backend/app.js
+++ backend/app.js
@@ -24,16 +24,17 @@ const auth = require('./routes/auth')
 const order = require('./routes/order')
 const payment = require('./routes/payment')
 
-app.get('/api/test', (req, res) => {
-  res.json({ success: true, message: 'Backend is live new new ew!' });
-});
-
-
 app.use('/api/v1/',products);
 app.use('/api/v1/',auth);
 app.use('/api/v1/',order);
 app.use('/api/v1/',payment);
 
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
+    app.get('*', (req, res) =>{
+        res.sendFile(path.resolve(__dirname, '../frontend/build/index.html'))
+    })
+}
 
 app.use(errorMiddleware)
 
