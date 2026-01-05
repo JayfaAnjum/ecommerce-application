b55afcc2e16d813830fc3a4c086ddb54d9781891
JayfaAnjum <jayfatha62@gmail.com>
2026-01-05 10:42:44 +0530

    donw:

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,18 +1,50 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-const connectDatabase = () => {
-    mongoose.connect(
+// const connectDatabase = () => {
+//     mongoose.connect(
+//         "mongodb+srv://saroth:saroth@cluster0.qvckxm3.mongodb.net/newproject?retryWrites=true&w=majority",
+//         {
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//             serverSelectionTimeoutMS: 5000 // optional, avoids hanging
+//         }
+//     ).then(con => {
+//         console.log(`MongoDB is connected to the host: ${con.connection.host}`);
+//     }).catch(err => {
+//         console.error('MongoDB connection error:', err);
+//     });
+// }
+
+// module.exports = connectDatabase;
+
+const mongoose = require("mongoose");
+
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
+const connectDatabase = async () => {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose
+      .connect(
         "mongodb+srv://saroth:saroth@cluster0.qvckxm3.mongodb.net/newproject?retryWrites=true&w=majority",
         {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            serverSelectionTimeoutMS: 5000 // optional, avoids hanging
+          bufferCommands: false,          // IMPORTANT
+          serverSelectionTimeoutMS: 5000,
         }
-    ).then(con => {
-        console.log(`MongoDB is connected to the host: ${con.connection.host}`);
-    }).catch(err => {
-        console.error('MongoDB connection error:', err);
-    });
-}
+      )
+      .then((mongoose) => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  console.log("MongoDB connected");
+  return cached.conn;
+};
 
 module.exports = connectDatabase;
