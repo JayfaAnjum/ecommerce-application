8517b3778fab5e244f579d5c51665fdb4c1bcbf8
JayfaAnjum <jayfatha62@gmail.com>
2026-01-07 18:11:13 +0530

    done

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -18,7 +18,7 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: [true, 'Please enter password'],
-        maxlength: [6, 'Password cannot exceed 6 characters'],
+        minlength: [8, 'Password must be at least 8 character'],
         select: false
     },
     avatar: {
